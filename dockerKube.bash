3dfd915c66da3d2c824f2fa1a47c98f0a8f7055f
Eliforbes42 <eliforbes42@gmail.com>
2018-10-11 21:01:04 -0700

    Initial install script
    docker, kube programs

---
+++ dockerKube.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get install -y kubelet=1.11.3-00 kubeadm=1.11.3-00 kubectl=1.11.3-00
+apt-mark hold kubelet kubeadm kubectl
