a8bcfa020628fc3d8a142d3f0effd0a98607daab
Eliforbes42 <eliforbes42@gmail.com>
2018-10-11 21:10:22 -0700

    Initial TIK Install Script
    TIK = Telegraf + InfluxDB + Kube-State-Metrics
    
    Kube-State-Metrics is assumed to be installed. This is kind of a misnomer..

---
+++ tikInstall.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+wget https://dl.influxdata.com/influxdb/releases/influxdb_1.4.0_amd64.deb
+sudo dpkg -i influxdb_1.4.0_amd64.deb
+sudo systemctl start influxdb
+wget https://dl.google.com/go/go1.10.4.linux-amd64.tar.gz 
+sudo tar -C /usr/local -xzf go1.10.4.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+export GOBIN=/usr/local/go/bin
+sudo curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+go get -d -v github.com/influxdata/telegraf
+sudo apt-get install -y make
+cd "$HOME/go/src/github.com/influxdata/telegraf"
+make
+sudo cp telegraf /usr/bin
+telegraf --input-filter prometheus --output-filter influxdb config > telegraf.conf
+echo "get ready to code..."
+
