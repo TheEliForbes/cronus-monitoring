de8057bbb82a9c0ee34220075fcdeddb7c0defe6
Eliforbes42 <eliforbes42@gmail.com>
2018-10-27 14:49:02 -0700

    Crazy Setup Scripting

---
+++ gtikk-charts/initHelm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if ![ -x "$(command -v helm)" ]; then
+	echo 'Helm is not installed.'
+	read -p "Do you wish to install Helm?" yn
+	case $yn in
+		[Yy]* ) chmod +x installHelm.sh; sudo ./installHelm.sh;;
+		[Nn]* ) exit;;
+		* ) echo "Please answer yes or no.";;
+	esac
+fi
+
+kubectl create -f rbac-config.yaml
+helm init --service-account tiller 
+
+read -p "Would you like to initialize the GKIT?" yn
+case $yn in
+	[Yy]* ) chmod +x create.sh; sudo ./create.sh;;
+	[Nn]* ) exit;;
+	* ) echo "Please answer yes or no.";;
+esac
