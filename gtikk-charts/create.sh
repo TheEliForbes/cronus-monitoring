1f01690384866b813ad5cd364a30c2d74941712e
Eliforbes42 <eliforbes42@gmail.com>
2018-10-27 12:58:23 -0700

    another reset...

--- gtikk-charts/create.sh
+++ gtikk-charts/create.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-helm install --name data --namespace kube-system ./influxdb/
-helm install --name polling --namespace kube-system ./telegraf-s/
-helm install --name hosts --namespace kube-system ./telegraf-ds/
-helm install --name alerts --namespace kube-system ./kapacitor/
-helm install --name dash --namespace kube-system ./grafana/
-#Replace Chronograf with Grafana
-#helm install --name dash --namespace tick ./chronograf/
-#kubectl get svc -w --namespace tick -l app=dash-chronograf
+helm install --name data --namespace tick ./influxdb/
+helm install --name polling --namespace tick ./telegraf-s/
+helm install --name hosts --namespace tick ./telegraf-ds/
+helm install --name alerts --namespace tick ./kapacitor/
+helm install --name dash --namespace tick ./chronograf/
+kubectl get svc -w --namespace tick -l app=dash-chronograf
