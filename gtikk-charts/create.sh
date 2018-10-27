773c8c74b25ac762e9108fe51b7f61c9489cf049
Eliforbes42 <eliforbes42@gmail.com>
2018-10-27 12:00:01 -0700

    Added secret insertion

--- gtikk-charts/create.sh
+++ gtikk-charts/create.sh
@@ -3,9 +3,9 @@
 helm install --name metrics --namespace kube-system ./kube-state-metrics/
 export KSM_URL="http://"$(sudo kubectl get svc --namespace kube-system metrics-kube-state-metrics -o json | jq -r .spec.clusterIP)":8085/metrics"
 helm install --name data --namespace kube-system ./influxdb/
-export IFDB_IP=$(sudo kubectl get svc --namespace kube-system data-influxdb -o json | jq -r .spec.clusterIP)
-helm install --name polling --namespace kube-system ./telegraf-s/
-helm install --name hosts --namespace kube-system ./telegraf-ds/
+export IFDB_URL="http://"$(sudo kubectl get svc --namespace kube-system data-influxdb -o json | jq -r .spec.clusterIP)":8086"
+helm install --name polling --set influxurl=$IFDB_URL --namespace kube-system ./telegraf-s/
+helm install --name hosts --set influxurl=$IFDB_URL --namespace kube-system ./telegraf-ds/
 helm install --name alerts --namespace kube-system ./kapacitor/
 helm install --name dash --namespace kube-system ./grafana/
 #Replace Chronograf with Grafana
