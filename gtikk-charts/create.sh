a03d3137260460a0e2ab90de04f37847130e5335
Eliforbes42 <eliforbes42@gmail.com>
2018-10-27 11:16:50 -0700

    KSM Host Variable

--- gtikk-charts/create.sh
+++ gtikk-charts/create.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 helm install --name metrics --namespace kube-system ./kube-state-metrics/
+export KSM_URL="http://"$(sudo kubectl get svc --namespace kube-system metrics-kube-state-metrics -o json | jq -r .spec.clusterIP)":8085/metrics"
 helm install --name data --namespace kube-system ./influxdb/
 helm install --name polling --namespace kube-system ./telegraf-s/
 helm install --name hosts --namespace kube-system ./telegraf-ds/
