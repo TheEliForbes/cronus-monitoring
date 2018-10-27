90a2a2ab706ef3037e7dd3b4f22fb872af4d057e
Eliforbes42 <eliforbes42@gmail.com>
2018-10-27 12:23:59 -0700

    Added continue prompts in script

--- gtikk-charts/create.sh
+++ gtikk-charts/create.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 helm install --name metrics --namespace kube-system ./kube-state-metrics/
+read -n 1 -p "Continue..." "mainmenuinput"
 #export KSM_URL="http://"$(sudo kubectl get svc --namespace kube-system metrics-kube-state-metrics -o json | jq -r .spec.clusterIP)":8085/metrics"
 helm install --name data --namespace kube-system ./influxdb/
+read -n 1 -p "Continue..." "mainmenuinput"
 #export IFDB_URL="http://"$(sudo kubectl get svc --namespace kube-system data-influxdb -o json | jq -r .spec.clusterIP)":8086"
 helm install --name polling --namespace kube-system ./telegraf-s/
+read -n 1 -p "Continue..." "mainmenuinput"
 helm install --name hosts --namespace kube-system ./telegraf-ds/
+read -n 1 -p "Continue..." "mainmenuinput"
 helm install --name alerts --namespace kube-system ./kapacitor/
+read -n 1 -p "Continue..." "mainmenuinput"
 helm install --name dash --namespace kube-system ./grafana/
 #Replace Chronograf with Grafana
 #helm install --name dash --namespace tick ./chronograf/
