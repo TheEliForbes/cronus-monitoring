2bb6710cc61b8af6f66353df480ea57e17f82db6
Eliforbes42 <eliforbes42@gmail.com>
2018-10-27 18:43:26 -0700

    readded namespacing

--- gtikk-charts/create.sh
+++ gtikk-charts/create.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-helm install --name metrics --service-account tiller --namespace kube-system ./kube-state-metrics/
+helm install --name metrics --tiller-namespace tick ./kube-state-metrics/
 read -n 1 -p "Continue..." "mainmenuinput"
-#export KSM_URL="http://"$(sudo kubectl get svc --namespace kube-system metrics-kube-state-metrics -o json | jq -r .spec.clusterIP)":8085/metrics"
-helm install --name data --service-account tiller --namespace kube-system ./influxdb/
-read -n 1 -p "Continue..." "mainmenuinput"
-#export IFDB_URL="http://"$(sudo kubectl get svc --namespace kube-system data-influxdb -o json | jq -r .spec.clusterIP)":8086"
-helm install --name polling --service-account tiller --namespace kube-system ./telegraf-s/
-read -n 1 -p "Continue..." "mainmenuinput"
-helm install --name hosts --service-account tiller --namespace kube-system ./telegraf-ds/
-read -n 1 -p "Continue..." "mainmenuinput"
-helm install --name alerts --service-account tiller --namespace kube-system ./kapacitor/
-read -n 1 -p "Continue..." "mainmenuinput"
-helm install --name dash --service-account tiller --namespace kube-system ./grafana/
+helm install --name data --tiller-namespace tick ./influxdb/
+helm install --name polling --tiller-namespace tick ./telegraf-s/
+helm install --name hosts --tiller-namespace tick ./telegraf-ds/
+helm install --name alerts --tiller-namespace tick ./kapacitor/
+helm install --name dash --tiller-namespace tick ./grafana/
 #Replace Chronograf with Grafana
 #helm install --name dash --namespace tick ./chronograf/
 #kubectl get svc -w --namespace tick -l app=dash-chronograf
