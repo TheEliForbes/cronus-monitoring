756ef5cde077f730fa6ea9021f14bafe569380ec
Eliforbes42 <eliforbes42@gmail.com>
2018-10-27 19:37:47 -0700

    roll forward

--- gtikk-charts/create.sh
+++ gtikk-charts/create.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-helm install --name metrics --namespace kube-system ./kube-state-metrics/
-helm install --name data --namespace kube-system ./influxdb/
-helm install --name polling --namespace kube-system ./telegraf-s/
-helm install --name hosts --namespace kube-system ./telegraf-ds/
-helm install --name alerts --namespace kube-system ./kapacitor/
-helm install --name dash --namespace kube-system ./grafana/
+helm install --name metrics --tiller-namespace tick --namespace tick ./kube-state-metrics/
+read -n 1 -p "Continue..." "mainmenuinput"
+helm install --name data --tiller-namespace tick --namespace tick ./influxdb/
+helm install --name polling --tiller-namespace tick --namespace tick ./telegraf-s/
+helm install --name hosts --tiller-namespace tick --namespace tickcreate.sh ./telegraf-ds/
+helm install --name alerts --tiller-namespace tick --namespace tick ./kapacitor/
+helm install --name dash --tiller-namespace tick --namespace tick ./grafana/
 #Replace Chronograf with Grafana
 #helm install --name dash --namespace tick ./chronograf/
 #kubectl get svc -w --namespace tick -l app=dash-chronograf
