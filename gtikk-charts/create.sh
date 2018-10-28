572259fa5c738c2544d5ae84bc98c92c003a42bb
Eliforbes42 <eliforbes42@gmail.com>
2018-10-27 18:47:51 -0700

    more namespacing

--- gtikk-charts/create.sh
+++ gtikk-charts/create.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-helm install --name metrics --tiller-namespace tick ./kube-state-metrics/
+helm install --name metrics --tiller-namespace tick --namespace tick ./kube-state-metrics/
 read -n 1 -p "Continue..." "mainmenuinput"
-helm install --name data --tiller-namespace tick ./influxdb/
-helm install --name polling --tiller-namespace tick ./telegraf-s/
-helm install --name hosts --tiller-namespace tick ./telegraf-ds/
-helm install --name alerts --tiller-namespace tick ./kapacitor/
-helm install --name dash --tiller-namespace tick ./grafana/
+helm install --name data --tiller-namespace tick --namespace tick ./influxdb/
+helm install --name polling --tiller-namespace tick --namespace tick ./telegraf-s/
+helm install --name hosts --tiller-namespace tick --namespace tickcreate.sh ./telegraf-ds/
+helm install --name alerts --tiller-namespace tick --namespace tick ./kapacitor/
+helm install --name dash --tiller-namespace tick --namespace tick ./grafana/
 #Replace Chronograf with Grafana
 #helm install --name dash --namespace tick ./chronograf/
 #kubectl get svc -w --namespace tick -l app=dash-chronograf
