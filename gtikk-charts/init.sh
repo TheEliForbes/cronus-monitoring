453089687a55c0d9b4da412405bb3e4656745892
Eliforbes42 <eliforbes42@gmail.com>
2018-10-27 17:51:35 -0700

    debugging ksm problems

--- gtikk-charts/init.sh
+++ gtikk-charts/init.sh
@@ -10,6 +10,16 @@ if ! [ -x "$(command -v helm)" ]; then
 	esac
 fi
 
+kubectl create -f rbac-config.yaml
+helm init --service-account tiller 
+
+read -p "Would you like to initialize the GKIT? (y/n)" yn
+case $yn in
+	[Yy]* ) chmod +x create.sh; sudo ./create.sh;;
+	[Nn]* ) exit;;
+	* ) echo "Please answer y/n.";;
+esac
+
 if ! [ -x "$(command -v jq)" ]; then
 	echo "JQ is not installed."
 	read -p "Do you wish to install JQ? (y/n)" yn
@@ -19,17 +29,5 @@ if ! [ -x "$(command -v jq)" ]; then
 		* ) echo "Please answer y/n."
 	esac
 fi
-		
-kubectl create -f rbac-config.yaml
-helm init --service-account tiller 
-
-: : : : : : : : : : : : : : :  #15 Nops to wait for tiller to set up
-
-read -p "Would you like to initialize the GKIT? (y/n)" yn
-case $yn in
-	[Yy]* ) chmod +x create.sh; sudo ./create.sh;;
-	[Nn]* ) exit;;
-	* ) echo "Please answer y/n.";;
-esac
 
 echo "Done. . ."
