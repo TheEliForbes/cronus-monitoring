d76232f0c509d793eff0711be4224e8d0583089d
Eliforbes42 <eliforbes42@gmail.com>
2018-10-28 17:19:35 -0700

    Query* Script Flagging Syntax

--- gtikk-charts/query.sh
+++ gtikk-charts/query.sh
@@ -3,7 +3,7 @@
 IP=$(sudo kubectl get svc --namespace kube-system data-influxdb -o json | jq -r .spec.clusterIP)
 URL="http://"$IP":8086"
 
-for opt in $@
+for $opt in $@
 	case $opt in
 		--databases | -d) echo "Showing Database List"; 
 						  curl $URL"/query?q=show+databases";;
@@ -12,3 +12,4 @@ for opt in $@
 		--ksm | -k) echo "Showing KSM Data";
 				    curl $URL"/query?q=select+*+from+telegraf..kube_pod_container_status_restarts_total";
 	esac
+done
