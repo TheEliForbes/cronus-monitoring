7fcc562672e6a7a6d9460772e4152cb677953e6e
Eliforbes42 <eliforbes42@gmail.com>
2018-10-28 23:26:01 -0700

    scripting errors

--- gtikk-charts/cleanUpTest.sh
+++ gtikk-charts/cleanUpTest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Clean up those pesky containers
 
-sudo kubectl delete pod --namespace kube-system data-influxdb-test-db
-sudo kubectl delete pod --namespace kube-system data-influxdb-test-tdb
-sudo kubectl delete pod --namespace kube-system metrics-kube-state-metrics-test-metrics
+sudo kubectl delete pod --namespace kube-system data-influxdb-service-test-db
+sudo kubectl delete pod --namespace kube-system data-influxdb-service-test-tdb
+sudo kubectl delete pod --namespace kube-system metrics-kube-state-metrics-service-test-metrics
