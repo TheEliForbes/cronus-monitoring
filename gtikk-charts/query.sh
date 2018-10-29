0f02cc6e93e751b24f87b95432475094d8c2206b
Eliforbes42 <eliforbes42@gmail.com>
2018-10-28 17:21:24 -0700

    Query* Script Flagging Syntax2

--- gtikk-charts/query.sh
+++ gtikk-charts/query.sh
@@ -6,10 +6,13 @@ URL="http://"$IP":8086"
 for $opt in $@
 	case $opt in
 		--databases | -d) echo "Showing Database List"; 
-						  curl $URL"/query?q=show+databases";;
+						  curl $URL"/query?q=show+databases";
+						  shift 1;;
 		--cpu | -c) echo "Showing CPU Data";
-				    curl $URL"/query?q=select+*+from+telegraf..cpu";;
+				    curl $URL"/query?q=select+*+from+telegraf..cpu";
+						  shift 1;;
 		--ksm | -k) echo "Showing KSM Data";
 				    curl $URL"/query?q=select+*+from+telegraf..kube_pod_container_status_restarts_total";
+						  shift 1;;
 	esac
 done
