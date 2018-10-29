f76ef52afa82288b2ab6d1087c445346edf982e2
Eliforbes42 <eliforbes42@gmail.com>
2018-10-28 17:15:30 -0700

    Curl Script Flagging

--- gtikk-charts/query.sh
+++ gtikk-charts/query.sh
@@ -3,11 +3,14 @@
 IP=$(sudo kubectl get svc --namespace kube-system data-influxdb -o json | jq -r .spec.clusterIP)
 URL="http://"$IP":8086"
 
-echo "Showing Database List"
-curl $URL"/query?q=show+databases"
-
-echo "Showing CPU Data"
-curl $URL"/query?q=select+*+from+telegraf..cpu"
-
-echo "Showing KSM Data"
-curl $URL"/query?q=select+*+from+telegraf..kube_pod_container_status_restarts_total"
+while [ ! $# -eq 0 ]
+do
+	case "$1" in
+		--databases | -d) echo "Showing Database List"; 
+						  curl $URL"/query?q=show+databases";;
+		--cpu | -c) echo "Showing CPU Data";
+				    curl $URL"/query?q=select+*+from+telegraf..cpu";;
+		--ksm | -k) echo "Showing KSM Data";
+				    curl $URL"/query?q=select+*+from+telegraf..kube_pod_container_status_restarts_total";
+	esac
+done
