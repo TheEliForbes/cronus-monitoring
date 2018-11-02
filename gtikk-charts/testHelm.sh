b50e3276571b1ccc8c2915fc1e784baefe476aab
Eliforbes42 <eliforbes42@gmail.com>
2018-11-01 21:20:43 -0700

    testHelm script flags

--- gtikk-charts/testHelm.sh
+++ gtikk-charts/testHelm.sh
@@ -5,10 +5,14 @@ helm test metrics
 helm test data
 chmod +x cleanUpTest.sh
 ./cleanUpTest.sh
+
+case $1 in
+	-n) true;;
+	* )read -p "Do you wish to clean up the tests? (y/n)" yn;
+	case $yn in
+		[Yy]* ) chmod +x cleanUpTest.sh; ./cleanUpTest.sh;;
+		[Nn]* ) exit;;
+		* ) echo "Please answer y/n.";;
+	esac;;
+esac
 echo "Done Testing!!!"
-#read -p "Do you wish to clean up the tests? (y/n)" yn
-#case $yn in
-#	[Yy]* ) chmod +x cleanUpTest.sh; ./cleanUpTest.sh;;
-#	[Nn]* ) exit;;
-#	* ) echo "Please answer y/n.";;
-#esac
