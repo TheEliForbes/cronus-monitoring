ac2ed5b5730e998b5323c3e6a840cc59c114baa1
Eliforbes42 <eliforbes42@gmail.com>
2018-11-01 21:02:23 -0700

    helm test script mod - no input required

--- gtikk-charts/testHelm.sh
+++ gtikk-charts/testHelm.sh
@@ -3,10 +3,11 @@
 
 helm test metrics
 helm test data
-
-read -p "Do you wish to clean up the tests? (y/n)" yn
-case $yn in
-	[Yy]* ) chmod +x cleanUpTest.sh; ./cleanUpTest.sh;;
-	[Nn]* ) exit;;
-	* ) echo "Please answer y/n.";;
-esac
+chmod +x cleanUpTest.sh
+./cleanUpTest.sh
+#read -p "Do you wish to clean up the tests? (y/n)" yn
+#case $yn in
+#	[Yy]* ) chmod +x cleanUpTest.sh; ./cleanUpTest.sh;;
+#	[Nn]* ) exit;;
+#	* ) echo "Please answer y/n.";;
+#esac
