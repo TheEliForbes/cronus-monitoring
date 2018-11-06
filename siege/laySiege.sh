5ee8844e236c6e98a1920d39d47f46c8513fd4fd
Eliforbes42 <eliforbes42@gmail.com>
2018-11-06 13:59:46 -0800

    swap siege urlFiles with args, using dynamic IPs

--- siege/laySiege.sh
+++ siege/laySiege.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+HOSTG=$(sudo kubectl get svc --namespace kube-system dash-grafana -o json | jq -r .spec.clusterIP)
+HOSTI=$(sudo kubectl get svc --namespace kube-system data-influxdb -o json | jq -r .spec.clusterIP)
+HOSTK=$(sudo kubectl get svc --namespace kube-system metrics-kube-state-metrics -o json | jq -r .spec.clusterIP)
+
 echo "We have them surrounded. . ."
 wait 1
 
-siege -f grafanaUrls.txt -c 5 -r 25 #5 users, 25reps
-siege -f influxUrls.txt -c 5 -r 25
-siege -f ksmUrls.txt -c 5 -r 25
+#5 users, 25reps
+siege \
+http://${HOSTG}/login?username=admin&password=strongpassword \
+http://${HOSTG}/dashboards 
+-c 5 -r 25 
+
+siege \
+http://${HOSTI}/query?q=show+databases \
+http://${HOSTI}/query?q=select+*+from+telegraf..cpu \
+http://${HOSTI}/query?q=select+*+from+telegraf..kube_pod_container_status_restarts_total \
+-c 5 -r 25
+
+siege \
+http://${HOSTK}/metrics \
+-c 5 -r 25
 
 echo "Siege has been completed. . ."
