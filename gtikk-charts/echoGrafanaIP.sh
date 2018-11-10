b8bf22ea1fb9ba0eb55f6de2eb5e568e1e8e6e8c
Eliforbes42 <eliforbes42@gmail.com>
2018-11-09 20:12:30 -0800

    Added start option in echoGrafanaIP.sh

--- gtikk-charts/echoGrafanaIP.sh
+++ gtikk-charts/echoGrafanaIP.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 echo "Grafana is at http://$(sudo kubectl get svc --namespace kube-system dash-grafana -o json | jq -r .spec.clusterIP)"
+
+case $1 in
+    --start | -s) firefox "http://$(sudo kubectl get svc --namespace kube-system dash-grafana -o json | jq -r .spec.clusterIP)"
+esac
