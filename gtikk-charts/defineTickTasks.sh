0cefad0bb08d1e6b0011876604cd1c9569f6315b
Eliforbes42 <eliforbes42@gmail.com>
2018-11-11 15:44:29 -0800

    manual task defining - copy to root

--- gtikk-charts/defineTickTasks.sh
+++ gtikk-charts/defineTickTasks.sh
@@ -3,5 +3,5 @@
 
 KPODNAME=$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .items[0].metadata.name }')
 kubectl exec -i -t --namespace kube-system $KPODNAME /bin/sh
-chmod +x /tmp/TICKscripts/defineTasks.sh;
-.//tmp/TICKscripts/defineTasks.sh
+chmod +x /TICKscripts/defineTasks.sh;
+.//TICKscripts/defineTasks.sh
