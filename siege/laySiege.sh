1cb0e8ad347aedc74b8c4f2533810e10ad1f6033
Eliforbes42 <eliforbes42@gmail.com>
2018-11-11 11:06:43 -0800

    laySiege; fix quotes, vars should work now

--- siege/laySiege.sh
+++ siege/laySiege.sh
@@ -8,15 +8,15 @@ echo "We have them surrounded. . ."
 sleep 1
 
 #5 users, 25reps
-siege 'http://$HOSTG/login?username=admin&password=strongpassword' 'http://$HOSTG/dashboards' -c 5 -r 25 
+siege "http://$HOSTG/login?username=admin&password=strongpassword" "http://$HOSTG/dashboards" -c 5 -r 25 
 sleep 1
 
 read -p "Continue? (enter)" c
-siege 'http://$HOSTI/query?q=show+databases' 'http://$HOSTI/query?q=select+*+from+telegraf..cpu' 'http://$HOSTI/query?q=select+*+from+telegraf..kube_pod_container_status_restarts_total' -c 5 -r 25
+siege "http://$HOSTI/query?q=show+databases" "http://$HOSTI/query?q=select+*+from+telegraf..cpu" "http://$HOSTI/query?q=select+*+from+telegraf..kube_pod_container_status_restarts_total" -c 5 -r 25
 sleep 1
 
 read -p "Continue? (enter)" c
-siege 'http://$HOSTK/metrics' -c 5 -r 25
+siege "http://$HOSTK/metrics" -c 5 -r 25
 sleep 1
 
 echo "Siege has been completed. . ."
