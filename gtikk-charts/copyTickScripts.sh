0c8a0a9c2aedb8d3602d0245ff57ee91b8d2bdeb
Eliforbes42 <eliforbes42@gmail.com>
2018-11-11 15:44:09 -0800

    manual task defining - copy to root

--- gtikk-charts/copyTickScripts.sh
+++ gtikk-charts/copyTickScripts.sh
@@ -4,4 +4,12 @@ KPODNAME=$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor
 echo $KPODNAME
 echo "$(pwd)/kapacitor/TICKscripts"
 echo "kube-system/$KPODNAME:/tmp"
-kubectl cp "$(pwd)/kapacitor/TICKscripts" "kube-system/$KPODNAME:/tmp"
+kubectl cp "$(pwd)/kapacitor/TICKscripts" "kube-system/$KPODNAME:/"
+
+read -p "Auto-define all alerts? (y/n)" yn
+case $yn in
+	[Yy]* ) chmod +x defineTickTasks.sh;
+			./defineTickTasks.sh;;
+	[Nn]* ) exit;;
+		* ) echo "Please answer y/n.";;
+esac
--- gtikk-charts/create.sh
+++ gtikk-charts/create.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
 helm install --name metrics --namespace kube-system ./kube-state-metrics/
-read -p "Continue with remaining components? (y/n)" yn
-case $yn in
-	[Yy]* ) helm install --name data --namespace kube-system ./influxdb/;
-			helm install --name polling --namespace kube-system ./telegraf-s/;
-			helm install --name hosts --namespace kube-system ./telegraf-ds/;
-			
-			helm install --name alerts --namespace kube-system ./kapacitor/;
-			helm install --name dash --namespace kube-system ./grafana/;;
-	[Nn]* ) exit;;
-		* ) echo "Please answer y/n.";;
-esac
+#read -p "Continue with remaining components? (y/n)" yn
+#case $yn in
+#	[Yy]* ) 
+helm install --name data --namespace kube-system ./influxdb/;
+helm install --name polling --namespace kube-system ./telegraf-s/;
+helm install --name hosts --namespace kube-system ./telegraf-ds/;
 
-read -p "Auto-define all alerts? (y/n)" yn
-case $yn in
-	[Yy]* ) chmod +x copyTickScripts.sh;
-			./copyTickScripts.sh;
-			chmod +x defineTickTasks.sh;
-			./defineTickTasks.sh;;
-	[Nn]* ) exit;;
-		* ) echo "Please answer y/n.";;
-esac
+helm install --name alerts --namespace kube-system ./kapacitor/;
+helm install --name dash --namespace kube-system ./grafana/;;
+#	[Nn]* ) exit;;
+#		* ) echo "Please answer y/n.";;
+#esac
