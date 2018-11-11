7c91d36e85e41268b84a4b21119a1343c11254b8
Eliforbes42 <eliforbes42@gmail.com>
2018-11-11 15:32:22 -0800

    manual task defining - make script sleep

--- gtikk-charts/copyTickScripts.sh
+++ gtikk-charts/copyTickScripts.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-KPODNAME=$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .items[0].metadata.name }');
-echo $KPODNAME;
-echo "$(pwd)/kapacitor/TICKscripts";
-echo "kube-system/$KPODNAME:/tmp";
-kubectl cp "$(pwd)/kapacitor/TICKscripts" "kube-system/$KPODNAME:/tmp"; 
+KPODNAME=$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .items[0].metadata.name }')
+echo $KPODNAME
+echo "$(pwd)/kapacitor/TICKscripts"
+echo "kube-system/$KPODNAME:/tmp"
+kubectl cp "$(pwd)/kapacitor/TICKscripts" "kube-system/$KPODNAME:/tmp"
+
+sleep 5
+
 kubectl exec -i -t --namespace kube-system $KPODNAME /bin/sh
 chmod +x /tmp/TICKscripts/defineTasks.sh;
 .//tmp/TICKscripts/defineTasks.sh
