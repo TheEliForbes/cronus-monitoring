473095436bff5b9dafc0ff0c1b0f6ce5fb0fd162
Eliforbes42 <eliforbes42@gmail.com>
2018-11-11 15:45:50 -0800

    manual task defining - create script fix

--- gtikk-charts/create.sh
+++ gtikk-charts/create.sh
@@ -4,12 +4,12 @@ helm install --name metrics --namespace kube-system ./kube-state-metrics/
 #read -p "Continue with remaining components? (y/n)" yn
 #case $yn in
 #	[Yy]* ) 
-helm install --name data --namespace kube-system ./influxdb/;
-helm install --name polling --namespace kube-system ./telegraf-s/;
-helm install --name hosts --namespace kube-system ./telegraf-ds/;
+helm install --name data --namespace kube-system ./influxdb/
+helm install --name polling --namespace kube-system ./telegraf-s/
+helm install --name hosts --namespace kube-system ./telegraf-ds/
 
-helm install --name alerts --namespace kube-system ./kapacitor/;
-helm install --name dash --namespace kube-system ./grafana/;;
+helm install --name alerts --namespace kube-system ./kapacitor/
+helm install --name dash --namespace kube-system ./grafana/
 #	[Nn]* ) exit;;
 #		* ) echo "Please answer y/n.";;
 #esac
