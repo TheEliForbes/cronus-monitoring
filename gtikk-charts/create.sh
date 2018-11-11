bf457b822667e82de82c9906d0b9c642a473a600
Eliforbes42 <eliforbes42@gmail.com>
2018-11-11 14:20:45 -0800

    manual task defining - change cp dir

--- gtikk-charts/create.sh
+++ gtikk-charts/create.sh
@@ -8,7 +8,7 @@ case $yn in
 			helm install --name hosts --namespace kube-system ./telegraf-ds/;
 			
 			helm install --name alerts --namespace kube-system ./kapacitor/;
-			kubectl cp ./TICKscripts /TICKscripts -c $(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .items[0].metadata.name }')
+			kubectl cp ./TICKscripts kube-system/$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .items[0].metadata.name }')/TICKscripts 
 			./connectToKapacitorContainer.sh
 			chmod +x /TICKscripts/defineTasks.sh
 			.//TICKscripts/defineTasks.sh
