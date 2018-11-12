81a4ef2e46271868e62e761ad36acdda9efb31bf
Eliforbes42 <eliforbes42@gmail.com>
2018-11-11 16:11:54 -0800

    manual task defining - string fix in script

--- gtikk-charts/copyTickScripts.sh
+++ gtikk-charts/copyTickScripts.sh
@@ -3,7 +3,7 @@
 KPODNAME=$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .items[0].metadata.name }')
 echo $KPODNAME
 echo "$(pwd)/kapacitor/TICKscripts"
-echo "kube-system/$KPODNAME:/tmp"
+echo "kube-system/$KPODNAME:/"
 kubectl cp "$(pwd)/kapacitor/TICKscripts" "kube-system/$KPODNAME:/"
 
 read -p "Auto-define all alerts? (y/n)" yn
