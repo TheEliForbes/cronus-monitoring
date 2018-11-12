c062c610a04d48703bd149a0ec16323dc9581043
Eliforbes42 <eliforbes42@gmail.com>
2018-11-12 00:16:50 -0800

    jenkins setup script- optioning

--- initJenkins.sh
+++ initJenkins.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #This script installs jenkins, starts it, and configures it.
 docker pull jenkinsci/blueocean
-docker run \
+
+read -p "Run Jenkins? (y/n)" yn
+case $yn in
+  [Yy]* ) docker run \
   -u root \
   --rm \
   -d \
@@ -9,17 +12,18 @@ docker run \
   -p 50000:50000 \
   -v jenkins-data:/var/jenkins_home \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  jenkinsci/blueocean > jenkinsPasswordFile.txt
+  jenkinsci/blueocean > jenkinsPasswordFile.txt;;
+esac
 
-  read -p "Install ssh server for jenkins? (y/n)" yn
-  case $yn in
-    [Nn]* ) exit;;
-  esac
-  
-  sudo apt-get install -y openssh-server
-  sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.factory-defaults
-  sudo chmod a-w /etc/ssh/sshd_config.factory-defaults
-  sudo systemctl restart ssh
-  mkdir ~/.ssh
-  chmod 700 ~/.ssh
-  ssh-keygen -t rsa
+read -p "Install ssh server for jenkins? (y/n)" yn
+case $yn in
+[Nn]* ) exit;;
+esac
+
+sudo apt-get install -y openssh-server
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.factory-defaults
+sudo chmod a-w /etc/ssh/sshd_config.factory-defaults
+sudo systemctl restart ssh
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+ssh-keygen -t rsa
