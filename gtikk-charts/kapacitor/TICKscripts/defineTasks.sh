4eeb048bd8f1de630ec0e7a5519d2865348b9f9a
Eliforbes42 <eliforbes42@gmail.com>
2018-11-11 16:03:14 -0800

    manual task defining - defineTasks.sh - actually get files

--- gtikk-charts/kapacitor/TICKscripts/defineTasks.sh
+++ gtikk-charts/kapacitor/TICKscripts/defineTasks.sh
@@ -2,7 +2,7 @@
 # This script is intended to auto-define the tasks included in TICKscripts/
 # This is necessary since the Kapacitor PR which attempts to better solve this, fails
 
-for alert in ./stream/; do
+for alert in ./stream/*.tick; do
     name="$(basename $alert)"
     kapacitor define $name \
         -type stream \
