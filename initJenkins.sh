069320b5bd751e47e437b1ad35f188b96386beab
Eliforbes42 <eliforbes42@gmail.com>
2018-11-12 19:47:19 -0800

    Named the Jenkins Container

--- initJenkins.sh
+++ initJenkins.sh
@@ -12,6 +12,7 @@ case $yn in
   -p 50000:50000 \
   -v jenkins-data:/var/jenkins_home \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  --name=jenkinscimaster \
   jenkinsci/blueocean > jenkinsPasswordFile.txt;;
 esac
 
