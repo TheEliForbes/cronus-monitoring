5c3b7233c277539ec14a2f62982c8c6aad8c6888
Eliforbes42 <eliforbes42@gmail.com>
2018-11-13 13:30:01 -0800

    build test commit2

--- scripts/query.sh
+++ scripts/query.sh
@@ -2,6 +2,7 @@
 # This has some standard queries for dev use.
 IP=$(sudo kubectl get svc --namespace kube-system data-influxdb -o json | jq -r .spec.clusterIP)
 URL="http://"$IP":8086"
+
 for opt in "$@"
 do
 	case $opt in
