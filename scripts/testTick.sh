7fbc7434f14b234c0fc7bb14ea7f31bec43a2911
Eliforbes42 <eliforbes42@gmail.com>
2018-11-19 12:22:52 -0800

    TickTests - proper script?

--- scripts/testTick.sh
+++ scripts/testTick.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kapacitor-unit -kapacitor http://$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .items[0].metadata.name }') -dir ../kapacitor/TICKscripts/stream -tests ../kapacitor/TICKscripts/tests/podstatus_test.yaml
+kapacitor-unit -kapacitor http://$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .items[0].metadata.name }') -dir ../gtikk-charts/kapacitor/TICKscripts/stream -tests ../gtikk-charts/kapacitor/TICKscripts/tests/podstatus_test.yaml
