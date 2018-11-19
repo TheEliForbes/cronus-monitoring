f1de5bfd698718a298502eccd2ecce7d813ba04e
Eliforbes42 <eliforbes42@gmail.com>
2018-11-19 12:32:31 -0800

    TickTests - script correction final

--- scripts/testTick.sh
+++ scripts/testTick.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-URL=$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .spec.clusterIP }')
+URL=$(sudo kubectl get svc --namespace kube-system alerts-kapacitor -o json | jq -r .spec.clusterIP)
 echo $URL:9092
 kapacitor-unit -kapacitor http://$URL:9092 -dir ../gtikk-charts/kapacitor/TICKscripts/stream -tests ../gtikk-charts/kapacitor/TICKscripts/tests/podstatus_test.yaml
