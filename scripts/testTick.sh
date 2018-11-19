47a36ff42380213495a2d60ec1219dc97bcac21e
Eliforbes42 <eliforbes42@gmail.com>
2018-11-19 12:30:47 -0800

    TickTests - script correction final?

--- scripts/testTick.sh
+++ scripts/testTick.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-URL=$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .items[0].metadata.name }')
+URL=$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .spec.clusterIP }')
 echo $URL:9092
 kapacitor-unit -kapacitor http://$URL:9092 -dir ../gtikk-charts/kapacitor/TICKscripts/stream -tests ../gtikk-charts/kapacitor/TICKscripts/tests/podstatus_test.yaml
