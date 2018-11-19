d5e636a302d2fa541a3bacb237d81b54874f316a
Eliforbes42 <eliforbes42@gmail.com>
2018-11-19 12:26:12 -0800

    TickTests - script correction

--- scripts/testTick.sh
+++ scripts/testTick.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-kapacitor-unit -kapacitor http://$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .items[0].metadata.name }') -dir ../gtikk-charts/kapacitor/TICKscripts/stream -tests ../gtikk-charts/kapacitor/TICKscripts/tests/podstatus_test.yaml
+URL=$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .items[0].metadata.name }')
+kapacitor-unit -kapacitor http://$URL:9092 -dir ../gtikk-charts/kapacitor/TICKscripts/stream -tests ../gtikk-charts/kapacitor/TICKscripts/tests/podstatus_test.yaml
