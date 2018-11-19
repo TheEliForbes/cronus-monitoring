685f6ef1168ba39ea986f49cc48b6976f017aee5
Eliforbes42 <eliforbes42@gmail.com>
2018-11-19 13:03:48 -0800

    TickTests - reclone script mod

--- scripts/reClone.sh
+++ scripts/reClone.sh
@@ -6,4 +6,5 @@ git clone https://github.com/Eliforbes42/TIKsm-setup
 chmod +x ~/TIKsm-setup/gtikk-charts/*.sh
 chmod +x ~/TIKsm-setup/selenium/*.sh
 chmod +x ~/TIKsm-setup/siege/*.sh
+chmod +x ~/TIKsm-setup/scripts/*.sh
 cd ~/TIKsm-setup/
