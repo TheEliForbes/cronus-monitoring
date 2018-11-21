7318b4024c1ea7dc305264f967c88a2a906001e6
Eliforbes42 <eliforbes42@gmail.com>
2018-11-21 12:07:26 -0800

    test script fix2

--- scripts/testTick.sh
+++ scripts/testTick.sh
@@ -2,4 +2,4 @@
 KURL=$(sudo kubectl get svc --namespace kube-system alerts-kapacitor -o json | jq -r .spec.clusterIP)
 IURL=$(sudo kubectl get svc --namespace kube-system data-influxdb -o json | jq -r .spec.clusterIP)
 echo $KURL:9092
-kapacitor-unit -kapacitor http://$KURL:9092 -influx http://$IURL:8086 -dir ../gtikk-charts/kapacitor/TICKscripts/stream -tests ../gtikk-charts/kapacitor/TICKscripts/tests/*.yaml
+kapacitor-unit -kapacitor http://$KURL:9092 -influxdb http://$IURL:8086 -dir ../gtikk-charts/kapacitor/TICKscripts/stream -tests ../gtikk-charts/kapacitor/TICKscripts/tests/*.yaml
