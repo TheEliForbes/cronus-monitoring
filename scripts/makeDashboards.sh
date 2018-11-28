59850daee4f459fae05de0131f97da7e22ea01df
Eliforbes42 <eliforbes42@gmail.com>
2018-11-27 20:50:27 -0800

    Sed Dashboard Generation - name fix

--- scripts/makeDashboards.sh
+++ scripts/makeDashboards.sh
@@ -3,5 +3,5 @@
 NODENAMES=$(kubectl get nodes -o jsonpath='{ .items[*].metadata.name }')
 for WATCHEDNODE in $NODENAMES
 do
-   sed -i "s/WATCHEDNODE/$WATCHEDNODE/g" dashboardTemplate.txt > "$WATCHEDNODE-system-metrics.json"
+   sed -i "s/WATCHEDNODE/$WATCHEDNODE/g" systemDashboardTemplate.txt > "$WATCHEDNODE-system-metrics.json"
 done
