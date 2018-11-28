549b6e19b8a7e16bafba6aae666f4582e65ea4c8
Eliforbes42 <eliforbes42@gmail.com>
2018-11-27 18:17:54 -0800

    Add & Update Selenium Tests

--- selenium/errorTest.py
+++ selenium/errorTest.py
@@ -1,5 +1,5 @@
-#login.py
-#To run this,  $ python login.py $IP
+#errorTest.py
+#To run this,  $ python errorTest.py $IP
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -22,3 +22,15 @@ password.send_keys("strongpassword")
 
 login_attempt = browser.find_element_by_xpath("//*[@type=\"submit\"]")
 login_attempt.click()
+
+# Click to Restarts Dashboard
+browser.get("http://%s/dashboards" % sys.argv[1])
+dashboard = browser.find_element_by_xpath("//*[@class=\"search-item search-item--indent\"][2]")
+dashboard.click()
+
+browser.get("http://%s/d/iyFav8Jik/restarts-dashboard?orgId=1" % sys.argv[1])
+time.sleep(2)
+infobox = browser.find_element_by_xpath("//*[@class=\"panel-info-corner-inner\"][1]") 
+assert infobox.value_of_css_property("color") == "rgb(142, 142, 142)"
+#print("Good color means no error. Color displayed below..")
+#print(infobox.value_of_css_property("color"))
