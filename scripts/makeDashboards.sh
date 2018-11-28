5c52ea2ea4ad05fd24175f1e69de825a6b6996b0
Eliforbes42 <eliforbes42@gmail.com>
2018-11-27 20:54:18 -0800

    Sed Dashboard Generation - flag fix

--- scripts/makeDashboards.sh
+++ scripts/makeDashboards.sh
@@ -3,5 +3,5 @@
 NODENAMES=$(kubectl get nodes -o jsonpath='{ .items[*].metadata.name }')
 for WATCHEDNODE in $NODENAMES
 do
-   sed -i "s/WATCHEDNODE/$WATCHEDNODE/g" systemDashboardTemplate.txt > "$WATCHEDNODE-system-metrics.json"
+   sed "s/WATCHEDNODE/$WATCHEDNODE/g" systemDashboardTemplate.txt > "$WATCHEDNODE-system-metrics.json"
 done
