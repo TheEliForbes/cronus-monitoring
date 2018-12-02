156aee6ac29184bf32a5bc53bcbb8287606a0e1a
Eliforbes42 <eliforbes42@gmail.com>
2018-12-01 17:24:21 -0800

    minor clarification

--- scripts/defineTickTasks.sh
+++ scripts/defineTickTasks.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# this script probably won't work..
-echo "this script probably won't work.."
+echo "this script may fail.."
 KPODNAME=$(sudo kubectl get pods --namespace kube-system -l app=alerts-kapacitor -o jsonpath='{ .items[0].metadata.name }')
 kubectl exec -i -t --namespace kube-system $KPODNAME /bin/sh
 chmod +x /TICKscripts/defineTasks.sh;
