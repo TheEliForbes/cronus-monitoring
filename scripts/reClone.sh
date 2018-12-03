c698ce7cc5adba78d82d2f5b212691c2c10bd512
Eliforbes42 <eliforbes42@gmail.com>
2018-12-03 12:55:03 -0800

    reClone script mod

--- scripts/reClone.sh
+++ scripts/reClone.sh
@@ -3,7 +3,6 @@
 
 sudo rm -r cronus-monitoring
 git clone https://github.com/Eliforbes42/cronus-monitoring
-chmod +x ~/cronus-monitoring/charts/*.sh
 chmod +x ~/cronus-monitoring/selenium/*.sh
 chmod +x ~/cronus-monitoring/siege/*.sh
 chmod +x ~/cronus-monitoring/scripts/*.sh
