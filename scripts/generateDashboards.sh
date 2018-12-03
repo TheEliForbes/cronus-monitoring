cde693a567bf4caf9e53f85fd6acdec948540f51
Jeff <thejeffwalsh@gmail.com>
2018-12-03 03:22:12 -0800

    debug echo statements

--- scripts/generateDashboards.sh
+++ scripts/generateDashboards.sh
@@ -4,12 +4,14 @@ NODENAMES=$(kubectl get nodes -o jsonpath='{ .items[*].metadata.name }')
 for NODE in $NODENAMES
 do
    if [ "$counter" != '0' ]; then 
+      echo "counter = $counter and NODE = $NODE"
       SYSNODE="$NODE-system"
       sed "s/WATCHEDNODE/$NODE/g" systemDashboardTemplate.txt > "$SYSNODE-metrics.json"
       NODEuuid=$(uuidgen)
       sed -i "s/\"uid\": \"UCRfGnBmk\"/\"uid\": \"$NODEuuid\" /g" "$SYSNODE-metrics.json"
    else 
       let "counter=counter+1"
+      echo "counter = $counter"
    fi    
 done
 
