aa463b3b965dc54186edc69e5dc7e6b5a330f635
Eliforbes42 <eliforbes42@gmail.com>
2018-12-03 15:14:01 -0800

    DashGen - create dashboard folder, remove old dashboards

--- scripts/generateDashboards.sh
+++ scripts/generateDashboards.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+echo "Removing previously generated dashboards. . ."
+rm ./generatedDashboards/*.json
+echo "Old Dashboards Removed."
+
 declare -i counter=0
 NODENAMES=$(kubectl get nodes -o jsonpath='{ .items[*].metadata.name }')
 
@@ -9,9 +14,9 @@ for NODE in $NODENAMES
 do
    if [ "$counter" != '0' ]; then 
       SYSNODE="$NODE-system"
-      sed "s/WATCHEDNODE/$NODE/g" systemDashboardTemplate.txt > "$SYSNODE-metrics.json"
+      sed "s/WATCHEDNODE/$NODE/g" systemDashboardTemplate.txt > "generatedDashboards/$SYSNODE-metrics.json"
       NODEuuid=$(uuidgen)
-      sed -i "s/\"uid\": \"UCRfGnBmk\"/\"uid\": \"$NODEuuid\" /g" "$SYSNODE-metrics.json"
+      sed -i "s/\"uid\": \"UCRfGnBmk\"/\"uid\": \"$NODEuuid\" /g" "generatedDashboards/$SYSNODE-metrics.json"
       echo "$SYSNODE-metrics.json created"
    else 
       let "counter=counter+1"
@@ -19,10 +24,10 @@ do
 done
 
 echo ""
-echo "System Dashboards generated"
+echo "System Dashboards generated -- see ./generatedDashboards"
 echo "Copying dashboards to cronus-monitoring/charts/grafana/dashhboards/"
 echo ". . ."
 
-cp *.json ../charts/grafana/dashboards/
+cp ./generatedDashboards/*.json ../charts/grafana/dashboards/
 
 echo "Dashboards copied to cronus-monitoring/charts/grafana/dashhboards/"
