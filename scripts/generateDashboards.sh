d7ac43c16cac511c1d18e29aceae9dc4ea712247
Jeff <thejeffwalsh@gmail.com>
2018-12-03 03:01:00 -0800

    removed master node from dashboard creation

--- scripts/generateDashboards.sh
+++ scripts/generateDashboards.sh
@@ -3,10 +3,12 @@
 NODENAMES=$(kubectl get nodes -o jsonpath='{ .items[*].metadata.name }')
 for NODE in $NODENAMES
 do
-   SYSNODE="$NODE-system"
-   sed "s/WATCHEDNODE/$NODE/g" systemDashboardTemplate.txt > "$SYSNODE-metrics.json"
-   NODEuuid=$(uuidgen)
-   sed -i "s/\"uid\": \"UCRfGnBmk\"/\"uid\": \"$NODEuuid\" /g" "$SYSNODE-metrics.json"
+   if [["$NODE" - "kube-master"]] then
+      SYSNODE="$NODE-system"
+      sed "s/WATCHEDNODE/$NODE/g" systemDashboardTemplate.txt > "$SYSNODE-metrics.json"
+      NODEuuid=$(uuidgen)
+      sed -i "s/\"uid\": \"UCRfGnBmk\"/\"uid\": \"$NODEuuid\" /g" "$SYSNODE-metrics.json"
+   fi
 done
 
 cp *.json ../charts/grafana/dashboards/
