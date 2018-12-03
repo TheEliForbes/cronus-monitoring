d75e94e71d9fb7470f50f744ba8f7a48e371bcc0
Jeff <thejeffwalsh@gmail.com>
2018-12-03 03:15:34 -0800

    lets try this one more time

--- scripts/generateDashboards.sh
+++ scripts/generateDashboards.sh
@@ -3,7 +3,7 @@ counter=0
 NODENAMES=$(kubectl get nodes -o jsonpath='{ .items[*].metadata.name }')
 for NODE in $NODENAMES
 do
-   if [$counter -ne 0]; then 
+   if ["$counter" != '0']; then 
       SYSNODE="$NODE-system"
       sed "s/WATCHEDNODE/$NODE/g" systemDashboardTemplate.txt > "$SYSNODE-metrics.json"
       NODEuuid=$(uuidgen)
