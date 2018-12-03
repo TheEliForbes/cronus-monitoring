c3aad6c22001bf6ce51454206f8e7322d2528e73
Jeff <thejeffwalsh@gmail.com>
2018-12-03 03:09:30 -0800

    attempting to remove master again

--- scripts/generateDashboards.sh
+++ scripts/generateDashboards.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
+counter=0
 NODENAMES=$(kubectl get nodes -o jsonpath='{ .items[*].metadata.name }')
 for NODE in $NODENAMES
 do
-   if [["$NODE" -ne "kube-master"]] then
+   if [counter==0] then 
+      let "counter=counter+1"
+   else
       SYSNODE="$NODE-system"
       sed "s/WATCHEDNODE/$NODE/g" systemDashboardTemplate.txt > "$SYSNODE-metrics.json"
       NODEuuid=$(uuidgen)
       sed -i "s/\"uid\": \"UCRfGnBmk\"/\"uid\": \"$NODEuuid\" /g" "$SYSNODE-metrics.json"
-   fi
 done
 
 cp *.json ../charts/grafana/dashboards/
