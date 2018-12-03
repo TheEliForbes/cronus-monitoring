95983ece79c7bb5da77e82d43938debdd3d5c371
Jeff <thejeffwalsh@gmail.com>
2018-12-03 03:12:50 -0800

    fixing syntax errors

--- scripts/generateDashboards.sh
+++ scripts/generateDashboards.sh
@@ -3,13 +3,14 @@ counter=0
 NODENAMES=$(kubectl get nodes -o jsonpath='{ .items[*].metadata.name }')
 for NODE in $NODENAMES
 do
-   if [counter==0] then 
-      let "counter=counter+1"
-   else
+   if [$counter -ne 0] then 
       SYSNODE="$NODE-system"
       sed "s/WATCHEDNODE/$NODE/g" systemDashboardTemplate.txt > "$SYSNODE-metrics.json"
       NODEuuid=$(uuidgen)
       sed -i "s/\"uid\": \"UCRfGnBmk\"/\"uid\": \"$NODEuuid\" /g" "$SYSNODE-metrics.json"
+   else 
+      let "counter=counter+1"
+   fi    
 done
 
 cp *.json ../charts/grafana/dashboards/
