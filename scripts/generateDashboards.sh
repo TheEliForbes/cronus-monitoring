14ac10ce72ef6db4da2f5d57371473658228eede
Eliforbes42 <eliforbes42@gmail.com>
2018-12-03 12:07:22 -0800

    Dashboard Generating - script echos

--- scripts/generateDashboards.sh
+++ scripts/generateDashboards.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 declare -i counter=0
 NODENAMES=$(kubectl get nodes -o jsonpath='{ .items[*].metadata.name }')
+
+echo "Generating System Dashboards for: $NODENAMES. . ."
+
 for NODE in $NODENAMES
 do
    if [ "$counter" != '0' ]; then 
@@ -8,9 +11,15 @@ do
       sed "s/WATCHEDNODE/$NODE/g" systemDashboardTemplate.txt > "$SYSNODE-metrics.json"
       NODEuuid=$(uuidgen)
       sed -i "s/\"uid\": \"UCRfGnBmk\"/\"uid\": \"$NODEuuid\" /g" "$SYSNODE-metrics.json"
+      echo "$SYSNODE-metrics.json created"
    else 
       let "counter=counter+1"
    fi    
 done
 
+echo "System Dashboards generated. . ."
+echo "Copying dashboards to cronus-monitoring/charts/grafana/dashhboards. . ."
+
 cp *.json ../charts/grafana/dashboards/
+
+echo "Dashboards copied. . ."
