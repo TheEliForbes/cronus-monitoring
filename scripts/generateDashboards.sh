675ae3b02a31650521cce9def7cbfc3f462bdcca
Jeff <thejeffwalsh@gmail.com>
2018-12-03 02:36:19 -0800

    possible sed hang fix

--- scripts/generateDashboards.sh
+++ scripts/generateDashboards.sh
@@ -3,13 +3,10 @@
 NODENAMES=$(kubectl get nodes -o jsonpath='{ .items[*].metadata.name }')
 for NODE in $NODENAMES
 do
-   case $1 in
-   --verbose |-v)    echo $NODE
-   esac
-      SYSNODE="$NODE-system"
-      sed "s/WATCHEDNODE/$SYSNODE/g" systemDashboardTemplate.txt > "$SYSNODE-metrics.json"
-      NODEuuid=$(uuidgen)
-      sed "s/\"uid\": \"UCRfGnBmk\"/\"uid\": \"$NODEuuid\" /g"
+   SYSNODE="$NODE-system"
+   sed -i "s/WATCHEDNODE/$SYSNODE/g" systemDashboardTemplate.txt > "$SYSNODE-metrics.json"
+   NODEuuid=$(uuidgen)
+   sed "s/\"uid\": \"UCRfGnBmk\"/\"uid\": \"$NODEuuid\" /g"
 done
 
 cp *.json ../charts/grafana/dashboards/
