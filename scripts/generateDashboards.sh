eb5706a1b4d0c0239ee765228fef30b17e481044
Jeff <thejeffwalsh@gmail.com>
2018-12-03 02:51:44 -0800

    fixed a mistake where it added -system to every query

--- scripts/generateDashboards.sh
+++ scripts/generateDashboards.sh
@@ -4,7 +4,7 @@ NODENAMES=$(kubectl get nodes -o jsonpath='{ .items[*].metadata.name }')
 for NODE in $NODENAMES
 do
    SYSNODE="$NODE-system"
-   sed "s/WATCHEDNODE/$SYSNODE/g" systemDashboardTemplate.txt > "$SYSNODE-metrics.json"
+   sed "s/WATCHEDNODE/$NODE/g" systemDashboardTemplate.txt > "$SYSNODE-metrics.json"
    NODEuuid=$(uuidgen)
    sed -i "s/\"uid\": \"UCRfGnBmk\"/\"uid\": \"$NODEuuid\" /g" "$SYSNODE-metrics.json"
 done
