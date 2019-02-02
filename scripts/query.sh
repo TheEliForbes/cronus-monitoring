716a2b9086f02a6d736a463664129d73f28c556e
Eliforbes42 <eliforbes42@gmail.com>
2019-02-01 16:06:47 -0800

    query.sh usage - spacing

--- scripts/query.sh
+++ scripts/query.sh
@@ -2,17 +2,15 @@
 # This has some standard queries for dev use.
 
 usage() {
-echo '\n./query.sh [-d | --databases] [-c | --cpu] [-k | --ksm] ["select+*+from+telegraf.."]\n'
-
+echo ""
+echo './query.sh [-d | --databases] [-c | --cpu] [-k | --ksm] ["select+*+from+telegraf.."]'
+echo ""
 echo "'-d' | '--databases'       - Show the databases that exist in Influx."
-
 echo "'-c' | '--cpu'             - Show all data from the 'cpu' table using 'select+*+from+telegraf..cpu'."
-
 echo "'-k' | '--ksm'             - Show all data from the 'kube_pod_container_status_restarts_total' table using a query similar to that used for 'cpu'."
-
 echo "'select+some+query+string' - Show data depending on the inputted IFQL query."
-
-echo '\nThis queries the Influx database for all supplied options, predefined or otherwise.'	
+echo ""
+echo 'This queries the Influx database for all supplied options, predefined or otherwise.'	
 }
 
 IP=$(sudo kubectl get svc --namespace kube-system data-influxdb -o json | jq -r .spec.clusterIP)
