37799da66df0b57766f752617d87127ae18c614c
ronaldbruce <ronald.bruce@wsu.edu>
2019-02-05 17:46:01 -0800

    fixing filepath on testTickScripts.sh

--- scripts/testTickScripts.sh
+++ scripts/testTickScripts.sh
@@ -2,5 +2,5 @@
 KURL=$(sudo kubectl get svc --namespace kube-system alerts-kapacitor -o json | jq -r .spec.clusterIP)
 IURL=$(sudo kubectl get svc --namespace kube-system data-influxdb -o json | jq -r .spec.clusterIP)
 echo $KURL:9092
-kapacitor-unit -kapacitor http://$KURL:9092 -influxdb http://$IURL:8086 -dir ../charts/kapacitor/TICKscripts/test_scripts -tests ../gtikk-charts/kapacitor/TICKscripts/tests/all_tests.yaml
+kapacitor-unit -kapacitor http://$KURL:9092 -influxdb http://$IURL:8086 -dir ../charts/kapacitor/TICKscripts/test_scripts -tests ../charts/kapacitor/TICKscripts/tests/all_tests.yaml
 
