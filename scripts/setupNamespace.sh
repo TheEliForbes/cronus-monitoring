2f6c0a3a560e919f1d2f228a4da7d91fb73c710a
Eliforbes42 <eliforbes42@gmail.com>
2019-02-28 13:37:24 -0800

    Uncomment methods for testing

--- scripts/setupNamespace.sh
+++ scripts/setupNamespace.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-if [ -n "$1" ]; then
-	$namespace = "$1"
-fi
 
 if [ -n "$1" ]; then
-    #kubectl create serviceaccount tiller --namespace $1
-    #echo "Replacing namespace values in Role Configuration Files"
-    #sed "s/NAMESPACE/$1/g" role-tiller-template.yaml > role-tiller.yaml
-    #sed "s/NAMESPACE/$1/g" rolebinding-tiller-template.yaml > rolebinding-tiller.yaml
-    #echo "Name replacement complete."
-    #echo "Creating Tiller Roles in namespace '$1'"
-    #kubectl create -f role-tiller.yaml
-    #kubectl create -f rolebinding-tiller.yaml
-    #helm init --service-account tiller --tiller-namespace $1
+    kubectl create serviceaccount tiller --namespace $1
+    echo "Replacing namespace values in Role Configuration Files"
+    sed "s/NAMESPACE/$1/g" role-tiller-template.yaml > role-tiller.yaml
+    sed "s/NAMESPACE/$1/g" rolebinding-tiller-template.yaml > rolebinding-tiller.yaml
+    echo "Name replacement complete."
+    echo "Creating Tiller Roles in namespace '$1'"
+    kubectl create -f role-tiller.yaml
+    kubectl create -f rolebinding-tiller.yaml
+    helm init --service-account tiller --tiller-namespace $1
 
     read -p "Set up stack using the given namespace?" yn
     case $yn in
      [Yy]* ) ./create.sh $1;;
     esac
 else
-    #echo "Setting up Tiller in default namespace (kube-system)"
-    #kubectl create serviceaccount tiller --namespace kube-system
-    #kubectl create -f rbac-config.yaml
-    #helm init --service-account tiller
+    echo "Setting up Tiller in default namespace (kube-system)"
+    kubectl create serviceaccount tiller --namespace kube-system
+    kubectl create -f rbac-config.yaml
+    helm init --service-account tiller
+
+    read -p "Set up stack?" yn
+    case $yn in
+     [Yy]* ) ./create.sh;;
+    esac
 fi
-
-
