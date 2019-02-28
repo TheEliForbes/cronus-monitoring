76c4b189100a473f99479134bea108a98af9784b
Eliforbes42 <eliforbes42@gmail.com>
2019-02-28 14:18:46 -0800

    curl fix

--- scripts/curl.sh
+++ scripts/curl.sh
@@ -10,8 +10,4 @@ curlCall() {
   curl "http://$(sudo kubectl get svc --namespace $namespace metrics-kube-state-metrics -o json | jq -r .spec.clusterIP):$1/metrics"
 }
 
-if [ -n "$1" ]; then
-	curlCall $1
-else
-	curlCall $PORT
-fi
+curlCall $PORT
