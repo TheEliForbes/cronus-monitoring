0e449c074534822c12aafc26b6b2229c203b9df2
Eliforbes42 <eliforbes42@gmail.com>
2019-03-22 10:31:01 -0700

    Namespace variable assignment fix

--- selenium/doorTest.sh
+++ selenium/doorTest.sh
@@ -2,7 +2,7 @@
 
 namespace="kube-system"
 if [ -n "$1" ]; then
-	$namespace = "$1"
+	$namespace="$1"
 fi
 
 python errorTest.py $(sudo kubectl get svc --namespace $namespace dash-grafana -o json | jq -r .spec.clusterIP)
