c47dc58148dd9dbb830bdae139d924bff34ddabf
Eliforbes42 <eliforbes42@gmail.com>
2019-03-27 16:03:48 -0700

    Remove unnecessary comments

--- scripts/init.sh
+++ scripts/init.sh
@@ -31,16 +31,6 @@ kubectl create -f rbac-config.yaml
 #  echo "Default RBAC Configuration already exists."
 #fi
 helm init --service-account tiller 
-# echo ""
-# echo "Please wait 30 seconds for Tiller to set up"
-# echo "------------------------------"
-# for ((i=30; i>0; i--))
-# do	
-#   echo -n "#"
-#   sleep 1
-# done
-# echo ""
-# echo "------------------------------"
 
 if ! [ -x "$(command -v jq)" ]; then
   echo "JQ is not installed."
@@ -50,8 +40,5 @@ fi
 
 echo "Installing Helm Unit-Test. . ."
 helm plugin install https://github.com/lrills/helm-unittest
-
-#echo "Initializing cronus-monitoring"
-#sudo ./create.sh
 
 echo "Done. . ."
